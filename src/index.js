31016745a39ab5eca4d15b2db3e26562ed62498c
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-10-08 16:41:51 -0700

    Información modificada y nuevos elementos

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ThemeContextProvider from './contexts/ThemeContext'
 
-
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
@@ -15,6 +14,5 @@ ReactDOM.render(
     </ThemeContextProvider>,
   document.getElementById('root')
 );
-
 
 reportWebVitals();

203401ba83f0d65e775bdca7f2b42abd9eed46c1
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-12-20 15:01:47 -0800

    New service

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -7,43 +7,43 @@ import { FaInternetExplorer,  FaChalkboardTeacher, FaCameraRetro, FaPinterest, F
  
 
 export const servicesData = [
-    // {
-    //     id: 1,
-    //     title: 'Marketing',
-    //     icon: <BiShoppingBag/>
-    // },
     {
         id: 1,
+        title: 'Marketing',
+        icon: <BiShoppingBag/>
+    },
+    {
+        id: 2,
         title: 'Content Writing',
         icon: <BiPencil /> 
     },
     {
-        id: 2,
+        id: 3,
         title: 'Web Design',
         icon: <BsCodeSlash />
     },
     {
-        id: 3,
+        id: 4,
         title: 'Video Editing',
         icon: <FaVideo />
     },
     {
-        id: 4,
+        id: 5,
         title: 'Web Development',
         icon: <FaDesktop />
     },
     {
-        id: 5,
+        id: 6,
         title: 'Mobile Development',
         icon: <FaMobile />
     },
     {
-        id: 6,
+        id: 7,
         title: 'UX/UI Design',
         icon: <FaPaintBrush />
     },
     {
-        id: 7,
+        id: 8,
         title: 'Virtual Assistant',
         icon: <FaUser />
     },

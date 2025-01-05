87f08cdf4ea4f1bb38fbc510d6dc41a49b84efec
Candy Velazco <a20490742@itmexicali.edu.mx>
2025-01-04 16:45:46 -0800

    About section

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "About me",
-    description1: "Hi! I'm Candy, a computer systems engineer specializing in web and mobile development, particularly in front-end technologies and UX/UI design.",
-    description2: " I also have experience in software analysis and documentation. I have a relentless drive for continuous improvement and a deep-seated passion for learning new things. I believe firmly in the power of technology and determination to solve everyday problems. I spent my leisure hours writing. Right now I'm trying a hand at AI and data analysis.",
+    description1: "Computer Systems Engineer with specialization in web and mobile development, data analysis and digital marketing. I'm a passionate problem solver and a lifelong learner.",
+    description2: "Expert in front-end technologies, mobile and web development, software analysis and technical documentation. I'm a quick learner and a team player. Right now I'm trying a hand at AI and data analysis.",
     image: 2
 }

77bc0a0a46c654049b0cfee06fabecdbd97ea94b
Candy Velazco <a20490742@itmexicali.edu.mx>
2025-01-20 12:28:16 -0800

    New certificate

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -2,6 +2,14 @@ export const achievementData = {
     achievements : [
         {
             id : 1,
+            title : 'Google Data Analytics Certificate',
+            details : 'his Certificate validates my proficiency in data analysis, demonstrating my ability to process, clean, and analyze data using tools like SQL, R, and spreadsheets. It highlights my expertise in creating data visualizations, generating insights, and supporting data-driven decision-making to solve business challenges.',
+            date : 'Jan 19, 2025',
+            field : 'Data Analysis',
+            image : 'https://cdn.shopaccino.com/igmguru/articles/Data-Analytics-Tutorial-1-900x558.jpg'
+        },
+        {
+            id : 2,
             title : 'Google UX Design Certificate',
             details : 'This Certificate validates my skills in user-centered design, showcasing my ability to create intuitive, accessible digital products through research, prototyping, and usability testing, helping businesses enhance user experiences.',
             date : 'Oct 07, 2024',
@@ -9,7 +17,7 @@ export const achievementData = {
             image : 'https://static.guvi.in/course-thumbnail/webps/figmaenglish.webp'
         },
         {
-            id : 2,
+            id : 3,
             title : 'AWS Academy Cloud Foundations',
             details : 'It validates my foundational knowledge of cloud computing concepts and AWS services. It showcases my ability to understand core AWS services, architecture, and cloud security.',
             date : 'May 28, 2023',

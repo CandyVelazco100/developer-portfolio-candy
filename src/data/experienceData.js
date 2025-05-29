e597cc7a84351833c57f470e71e0c706e4ea755a
Candy Velazco <a20490742@itmexicali.edu.mx>
2025-05-28 23:34:27 -0700

    New job

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,13 +1,20 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'Spark Technologies',
+        jobtitle: 'Software Developer',
+        startYear: 'June 2025',
+        endYear: ''
+    },
+    {
+        id: 2,
         company: 'TECNOLOchicas',
         jobtitle: 'Instructor',
         startYear: 'July 2024',
         endYear: ''
     },
     {
-        id: 2,
+        id: 3,
         company: 'Fixer MX',
         jobtitle: 'Frontend Developer',
         startYear: 'July 2024',
